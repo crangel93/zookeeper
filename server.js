e27d0f22e763b9ccbb16efc9854013a0e519e9b2
Christian Rangel <budrangel@gmail.com>
2022-09-14 15:45:03 -0700

    search for id

--- server.js
+++ server.js
@@ -58,6 +58,21 @@ app.get('/api/animals',(req,res)=>{
 
 res.json(results);
 });
+function findById(id,animalsArray){
+const result=animalsArray.filter(animal=>animal.id===id[0]);
+
+return result;
+}
+app.get('/api/animals/:id',(req,res)=>{
+const result=findById(req.params.id,animals);
+if(result){
+    res.json(result);
+}else{
+    res.send(404);
+}
+
+
+})
 app.listen(PORT,()=>{
 
 
